c3c7043a662f6db428c5148459ffea822eb56871
Travis <tssund93@gmail.com>
2020-11-19 11:31:20 -0600

    problem 1 bonus

--- 20201118_problem1.d
+++ 20201118_problem1.d
@@ -18,3 +18,19 @@ bool addsUpTo(int[] nums, int check) {
     }
     return false;
 }
+
+unittest {
+    assert([10,15,3,7].addsUpToBonus(17));
+    assert(![10,15,3,8].addsUpToBonus(17));
+}
+
+bool addsUpToBonus(int[] nums, int check) {
+    void[0][int] map;
+    foreach (num; nums) {
+        if (num !in map)
+            map[check - num] = [];
+        else
+            return true;
+    }
+    return false;
+}
