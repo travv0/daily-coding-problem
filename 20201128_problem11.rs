4fc363ff1ca86d5e930aad449962837e583c5a3f
Travis <tssund93@gmail.com>
2020-11-28 16:31:02 -0600

    refactor problem 11

--- 20201128_problem11.rs
+++ 20201128_problem11.rs
@@ -3,20 +3,20 @@
 use std::collections::HashMap;
 
 fn main() {
-    let tree = process_words(&["dog", "deer", "deal"]);
-    let result = tree.get_words("de");
+    let tree = CompletionTree::from(&["dog", "deer", "deal"]);
+    let result = tree.get_completions("de");
     assert_eq!(2, result.len());
     assert!(result.contains(&"deer".to_string()));
     assert!(result.contains(&"deal".to_string()));
 }
 
 #[derive(Debug)]
-struct Tree {
+struct CompletionTree {
     value: Option<String>,
-    next: HashMap<char, Tree>,
+    next: HashMap<char, Self>,
 }
 
-impl Tree {
+impl CompletionTree {
     fn new() -> Self {
         Self {
             next: HashMap::new(),
@@ -24,47 +24,47 @@ impl Tree {
         }
     }
 
-    fn _add(&mut self, word: &str, s: &str) {
-        if let Some(c) = s.chars().next() {
-            let tree = self.next.entry(c).or_insert_with(Tree::new);
-            tree._add(word, &s.chars().skip(1).collect::<String>());
-        } else {
-            self.value = Some(word.to_string());
-        };
+    fn from(words: &[&str]) -> Self {
+        let mut tree = Self::new();
+        for word in words {
+            tree.add(word);
+        }
+        tree
     }
 
     fn add(&mut self, word: &str) {
-        self._add(word, word);
+        fn add(tree: &mut CompletionTree, word: &str, s: &str) {
+            if let Some(c) = s.chars().next() {
+                let mut tree = tree.next.entry(c).or_insert_with(CompletionTree::new);
+                add(&mut tree, word, &s.chars().skip(1).collect::<String>());
+            } else {
+                tree.value = Some(word.to_string());
+            };
+        }
+
+        add(self, word, word);
     }
 
-    fn get_words(&self, s: &str) -> Vec<String> {
+    fn get_completions(&self, s: &str) -> Vec<String> {
         let mut node = self;
         for c in s.chars() {
-            node = match node.next.get(&c) {
-                Some(tree) => tree,
+            match node.next.get(&c) {
+                Some(tree) => node = tree,
                 None => return vec![],
             }
         }
-        let mut rest = node
+        node.flatten()
+    }
+
+    fn flatten(&self) -> Vec<String> {
+        let mut result = self
             .next
             .values()
-            .flat_map(|tree| tree.get_words(""))
-            .collect();
-        match &node.value {
-            Some(word) => {
-                let mut v = vec![word.to_string()];
-                v.append(&mut rest);
-                v
-            }
-            None => rest,
-        }
+            .flat_map(Self::flatten)
+            .collect::<Vec<_>>();
+        if let Some(word) = &self.value {
+            result.push(word.to_string());
+        };
+        result
     }
-}
-
-fn process_words(words: &[&str]) -> Tree {
-    let mut tree = Tree::new();
-    for word in words {
-        tree.add(word);
-    }
-    tree
 }
